ec87b81b1ac4a61f1a502e6a60c861e8fb95e282
Daniel <hydchongca@gmail.com>
2019-07-13 13:43:18 -0400

    first push

---
+++ SportsStore/Controllers/CartController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using SportsStore.Infrastructure;
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+        private Cart cart;
+
+        public CartController(IProductRepository repo, Cart cartService)
+        {
+            repository = repo;
+            cart = cartService;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                //Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                //SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                //Cart cart = GetCart();
+                cart.RemoveLine(product);
+                //SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        //replaced by SessionCart chap.10
+        //private void SaveCart(Cart cart)
+        //{
+        //    HttpContext.Session.SetJson("Cart", cart);
+        //}
+
+        //private Cart GetCart()
+        //{
+        //    Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+
+        //    return cart;
+        //}
+    }
+}
